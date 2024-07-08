746383d01f8e31cd9d4fe8c0d7cf646837d95ded
MrAlM <81728811+ALMAZIK17364@users.noreply.github.com>
2024-07-08 14:12:16 +0800

    Add files via upload
    Практическое задание по уроку "Базовые структуры данных"

---
+++ PracticBasicDataStructures.py
@@ -0,0 +1,14 @@
+#1st program
+print(9**0.5*5)
+#2nd program
+print(9.99>9.98 and 1000 != 1000.1)
+#3rd program
+a = 1234
+b = 5678
+print(((a%1000)//10)+((b%1000)//10))
+#4th program
+fn = 13.42
+sn = 42.13
+sfn = str(fn).split(".")
+ssn = str(sn).split(".")
+print(int(sfn[0])==int(ssn[1]) or int(sfn[1])==int(ssn[0]))
